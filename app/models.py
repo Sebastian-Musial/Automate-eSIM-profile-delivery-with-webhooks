89738b3936e6edbda85fabc3e053fd86ff952c7d
Sebastian Musial <dev.sebas.musial@gmail.com>
2026-06-11 01:37:44 +0200

    Create SQLModel for EsimProfile

---
+++ app/models.py
@@ -0,0 +1,19 @@
+from sqlmodel import Field, SQLModel
+from enum import Enum
+from datetime import datetime
+
+#ENUM
+class EsimStatus(str, Enum):
+    AVAILABLE  = "available"
+    ASSIGNED = "assigned"
+
+
+class EsimProfile(SQLModel, table=True):
+    __tablename__ = "esim_profiles"
+
+    id: int | None = Field(default=None, primary_key=True)
+    qr_code_value: str 
+    qr_image_url: str 
+    status: EsimStatus = Field(default=EsimStatus.available)
+    assigned_order_id: str | None = Field(default=None)
+    assigned_at: datetime | None = Field(default=None)
